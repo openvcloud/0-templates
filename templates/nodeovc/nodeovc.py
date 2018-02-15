583ec13722ebf8e4a9b525918bf9971ae1a9f5b8
katia-e <ekaterina@gig.tech>
2018-02-15 18:05:17 +0000

    add basic actions for vm

--- templates/nodeovc/nodeovc.py
+++ templates/nodeovc/nodeovc.py
@@ -9,6 +9,7 @@ class Nodeovc(TemplateBase):
     ACCOUNT_TEMPLATE = 'github.com/openvcloud/0-templates/account/0.0.1'
     VDCUSER_TEMPLATE = 'github.com/openvcloud/0-templates/vdcuser/0.0.1'
     OVC_TEMPLATE = 'github.com/openvcloud/0-templates/openvcloud/0.0.1'
+    SSH_TEMPLATE = 'github.com/openvcloud/0-templates/sshkey/0.0.1'
 
     def __init__(self, name, guid=None, data=None):
         super().__init__(name=name, guid=guid, data=data)
@@ -16,32 +17,38 @@ class Nodeovc(TemplateBase):
         self.data['name'] = name
         self._ovc = None
 
-
     def validate(self):
+        # Get object for an OVC service, make sure exactly one is running
         ovcs = self.api.services.find(template_uid=self.OVC_TEMPLATE, name=self.data.get('openvcloud', None))
-
         if len(ovcs) != 1:
             raise RuntimeError('found %s openvcloud connections, requires exactly 1' % len(ovcs))
 
         self.data['openvcloud'] = ovcs[0].name
         
+        # Get object for an account service, make sure exactly one is running
         accounts = self.api.services.find(template_uid=self.ACCOUNT_TEMPLATE, name=self.data.get('account', None))
-
-        if len(ovcs) != 1:
+        if len(accounts) != 1:
             raise RuntimeError('found %s openvcloud connections, requires exactly 1' % len(ovcs))
 
         self.data['account'] = accounts[0].name
 
+        # Get loccation
         locations = self.ovc.locations
-
         if len(locations) != 1:
             raise RuntimeError('found %s openvcloud locations, requires exactly 1' % len(locations))
 
         self.data['location'] = locations[0]['name']
 
-        sshkeypaths = j.clients.ssh.ssh_keys_list_from_agent()
-        sshkeypath = sshkeypaths[0]
-        self.sshkeyname = sshkeypath.split('/')[-1]  
+        # Get a path of the ssh-key uploaded to the ssh-agent
+        sshkeys = self.api.services.find(template_uid=self.SSH_TEMPLATE)
+        if len(sshkeys) != 1:
+            raise RuntimeError('found %s ssh services, requires exactly 1' % len(sshkeys))
+
+        # Get key name
+        self.data['sshkeyname'] = sshkeys[0].data['path'].split('/')[-1]
+
+    def update_data(self):
+        pass
 
     @property
     def ovc(self):
@@ -56,39 +63,88 @@ class Nodeovc(TemplateBase):
 
     def install(self):        
         machine = self.machine
+        if not machine:
+            self._machine_create()
+
         self._configure_ports(machine)
 
     @property
-    def machine(self):
-        
-        client = self.ovc
-
-        space = client.space_get(accountName=self.data['account'],
+    def space(self):
+        return self.ovc.space_get(accountName=self.data['account'],
                                  spaceName=self.data['openvcloud'],
                                  location=self.data['location'])
 
-        if space.machines.get(self.data['name']):
-            return space.machines.get(self.data['name'])
+    @property
+    def machine(self):
+        return self.space.machines.get(self.data['name'])
 
-        machine = space.machine_create(name=self.data['name'],
-                                       sshkeyname=self.sshkeyname,
-                                            )
-                #                            image=self.data['osImage'],
-                #                            memsize=self.data['memsize'],
-                                            # vcpus=self.data['vcpus'],
-                                            # disksize=self.data['bootDiskSize'],
-                                            # datadisks=self.data['disks'],
-                                            # sizeId=self.data['sizeId']
-        return machine
+    def _machine_create(self):
+        return self.space.machine_create(
+                name=self.data['name'],
+                sshkeyname= self.data['sshkeyname'],
+        )
+                # image=self.data['osImage'],
+                # memsize=self.data['memsize'],
+                # vcpus=self.data['vcpus'],
+                # disksize=self.data['bootDiskSize'],
+                # datadisks=self.data['disks'],
+                # sizeId=self.data['sizeId'
 
     def _configure_ports(self, machine):
-        import ipdb; ipdb.set_trace()
-        for port in self.data['ports']:
-            machine.portforward_create(
-                publicport=port['destination'], 
-                localport=port['source'], 
-                protocol='tcp',
-                )
-        pass
 
+        # get list of existing ports at the vm
+        existent_ports = [port['publicPort'] for port in machine.portforwards]
+
+        for port in self.data['ports']:
+            # check if ports do not exist yet
+            if port['destination'] not in existent_ports:
+                # create portforward
+                machine.portforward_create(
+                    publicport=port['destination'], 
+                    localport=port['source'], 
+                    protocol='tcp',
+                    )
+
+    def uninstall(self):
+        space = self.space
+
+        # check if the machine is in the space
+        if self.machine:
+            self.machine.delete()
+
+    def start(self):
+        machine = self.machine
+#        _check_ssh_authorization(job.service, machine)
+        machine.start()
+
+
+    def stop(self):
+        machine = self.machine
+#        _check_ssh_authorization(job.service, machine)
+        machine.stop()
+
+
+    def restart(self):
+        machine = self.machine
+#        _check_ssh_authorization(job.service, machine)
+        machine.restart()
+
+
+    def pause(self):
+        machine = self.machine
+#        _check_ssh_authorization(job.service, machine)
+        machine.pause()
+
+
+    def resume(self):
+        machine = self.machine
+#        _check_ssh_authorization(job.service, machine)
+        machine.resume()
+
+
+    def reset(self):
+        machine = self.machine
+#        _check_ssh_authorization(job.service, machine)
+        machine.reset()    
+        
 
