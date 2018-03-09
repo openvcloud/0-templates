0404ecf4d0fc07ad72177d16d284d12c605e058e
katia-e <ekaterina@gig.tech>
2018-03-09 17:46:06 +0000

    check passphrase and sshkey match

--- templates/sshkey/sshkey.py
+++ templates/sshkey/sshkey.py
@@ -1,3 +1,4 @@
+import paramiko
 from js9 import j
 from zerorobot.template.base import TemplateBase
 
@@ -18,10 +19,11 @@ class Sshkey(TemplateBase):
 
         if len(passphrase) < 5:
             raise ValueError('passphrase must be min of 5 characters')
-
         path = j.sal.fs.joinPaths(dir, name)
         if not j.sal.fs.exists(path):
             j.clients.sshkey.key_generate(path, passphrase=passphrase, overwrite=True, returnObj=False)
+        else:
+            paramiko.RSAKey.from_private_key_file(path, password=passphrase)
 
         j.clients.sshkey.get(
             name,
@@ -35,13 +37,4 @@ class Sshkey(TemplateBase):
     def install(self):
         pass
 
-    # def update_data(self, data):
-    #     if self.data['path'] == data['path']:
-    #         return
-
-    #     old = self.data['path']
-    #     self.data.update(data)
-    #     self.save()  # making sure data is not breaking the schema before we do key unload
-
-    #     j.clients.ssh.ssh_key_unload(old)
-    #     j.clients.ssh.load_ssh_key(self.data['path'])
+    
