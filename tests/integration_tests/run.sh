a02bd78c25cf0b5338d5fc0bf550d1b7c90e86bc
john-kheir <kheirj@codescalers.com>
2018-03-20 16:21:46 +0200

    fix run.sh

--- tests/integration_tests/run.sh
+++ tests/integration_tests/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 ## Cloning the framework
-git clone -C /tmp -b master https://github.com/0-complexity/G8_testing.git
+git -C /tmp clone -b master https://github.com/0-complexity/G8_testing.git
 cp -r /tmp/G8_testing/functional_testing/zrobot_templates/framework .
-
+rm -rf /tmp/G8_testing
 
 ## running testsuite
 nosetests -v -s testsuite --tc-file=config.ini
-
