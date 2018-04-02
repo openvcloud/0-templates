3806ce1ecd318852551a20de7d5c18b7699e9f84
john-kheir <kheirj@codescalers.com>
2018-04-02 10:25:22 +0200

    fix5

--- tests/integration_tests/travis.sh
+++ tests/integration_tests/travis.sh
@@ -10,12 +10,13 @@ if [[ ${action} == "before" ]]; then
     python3 packet_script.py -a create_machine -t ${packet_token} -k ${TRAVIS_JOB_NUMBER}
 
     echo "[+] Sending setup script to packet machine ..."
-    export ctrl_ipaddress=$(cat /tmp/device_ipaddress.txt)
+    ctrl_ipaddress=$(cat /tmp/device_ipaddress.txt)
     scp -o StrictHostKeyChecking=no packet_setup.sh root@${ctrl_ipaddress}:/root/
     ssh -t -o StrictHostKeyChecking=no root@${ctrl_ipaddress} "bash packet_setup.sh ${js9_branch} ${zrobot_branch} ${zerotier_network} ${zerotier_token} ${ctrl_zt_ipaddress} ${environment}"
     scp -o StrictHostKeyChecking=no prepare.sh root@${ctrl_ipaddress}:/root/
 
 elif [[ ${action} == "run" ]]; then
+    ctrl_ipaddress=$(cat /tmp/device_ipaddress.txt)
     ssh -t -o StrictHostKeyChecking=no root@${ctrl_ipaddress} "bash prepare.sh -d -s -r testsuite/ovc_tests/a_basic/accounts_tests.py"
 
 elif [[ ${action} == "teardown" ]]; then
