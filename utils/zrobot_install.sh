9144fb8437a9677232bfd525f8d311866056cba3
katia-e <ekaterina@gig.tech>
2018-05-14 13:36:04 +0000

    fix build

--- utils/zrobot_install.sh
+++ utils/zrobot_install.sh
@@ -6,10 +6,10 @@ export BRANCH="master"
 
 mkdir -p /opt/code/github/zero-os
 pushd /opt/code/github/zero-os
-cd /opt/code/github/zero-os
+
 # cloning source code
 git clone --depth=1 -b ${BRANCH} https://github.com/zero-os/0-robot.git
 pushd 0-robot
-pip3 install -r requirements.txt
-pip3 install - e .
+
+pip3 install -e .
 popd
