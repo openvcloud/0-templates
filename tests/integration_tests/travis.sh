c271fa6501e2856e1b8e0e11094009a15b9e29c0
john-kheir <kheirj@codescalers.com>
2018-05-21 14:49:00 +0200

    adding travis job to run openvcloud 0-templates

---
+++ tests/integration_tests/travis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script is used when using travis to run the testsuite
+action=$1
+
+if [[ ${action} == "setup" ]]; then
+    echo "[+] Generating ssh key ..."
+    ssh-keygen -f ~/.ssh/id_rsa -P ''
+
+    echo "[+] Joining zerotier network: ${zerotier_network}"
+    sudo zerotier-cli join ${zerotier_network}; sleep 10
+
+    echo "[+] Authorizing zerotier member"
+    memberid=$(sudo zerotier-cli info | awk '{print $3}')
+    curl -s -H "Content-Type: application/json" -H "Authorization: Bearer ${zerotier_token}" -X POST -d '{"config": {"authorized": true}}' https://my.zerotier.com/api/network/${zerotier_network}/member/${memberid} > /dev/null
+
+    for i in {1..20}; do
+        ping -c1 ${ctrl_zt_ipaddress} > /dev/null && break
+    done
+    if [ $? -gt 0 ]; then
+        echo "Can't reach the controller using this ip address ${ctrl_zt_ipaddress}"; exit 1
+    fi
+    sudo echo "${ctrl_zt_ipaddress}  ${environment}" >> /etc/hosts
+
+elif [[ ${action} == "run" ]]; then
+    bash prepare.sh -d -s -r testsuite/ovc_tests/a_basic/accounts_tests.py
+
+fi
