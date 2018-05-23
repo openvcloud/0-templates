687fef487e71c41d5db60e9b09ff60fa4ca542b7
john-kheir <kheirj@codescalers.com>
2018-05-23 14:30:27 +0200

    add id_rsa in /root

--- utils/jumspcale_install.sh
+++ utils/jumspcale_install.sh
@@ -4,7 +4,7 @@ set -e
 # settings
 export BRANCH=${1:-development}
 
-for target in /usr/local /opt /opt/cfg /opt/code/github/jumpscale /opt/var/capnp /opt/var/log $HOME/js9host/cfg; do
+for target in /root /usr/local /opt /opt/cfg /opt/code/github/jumpscale /opt/var/capnp /opt/var/log $HOME/js9host/cfg; do
     mkdir -p $target
     sudo chown -R $USER:$USER $target
 done
@@ -26,13 +26,14 @@ popd
 
 
 # create ssh key for jumpscale config manager
-mkdir -p ~/.ssh
-ssh-keygen -f ~/.ssh/id_rsa -P ''
+mkdir -p /root/.ssh
+ssh-keygen -f /root/.ssh/id_rsa -P ''
 eval `ssh-agent -s`
-ssh-add ~/.ssh/id_rsa
+ssh-add -D
+ssh-add /root/.ssh/id_rsa
 
 # initialize jumpscale config manager
 mkdir -p /opt/code/config_test
 git init /opt/code/config_test
 touch /opt/code/config_test/.jsconfig
-js9_config init --silent --path /opt/code/config_test/ --key ~/.ssh/id_rsa
+js9_config init --silent --path /opt/code/config_test/ --key /root/.ssh/id_rsa
